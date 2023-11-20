ad2f3180f7eaa68f6bd00bec570525b5c429a8a3
Iulian Marcu <marcu.iulian13@gmail.com>
2023-11-20 01:02:51 +0000

    WIP

--- data_structures/skip_list/skip_list.go
+++ data_structures/skip_list/skip_list.go
@@ -7,21 +7,7 @@ import (
 	"math/rand"
 )
 
-const P = 0.75
-
-type Node[K cmp.Ordered, T any] struct {
-	Value T
-	Key   K
-	Next  []*Node[K, T]
-}
-
-func (n *Node[K, T]) String() string {
-	return fmt.Sprintf("[Node] level: %v value %v", n.Level(), n.Value)
-}
-
-func (n *Node[K, T]) Level() int {
-	return len(n.Next) - 1
-}
+const P = 0
 
 func should_grow() bool {
 	return rand.Float32() < P
@@ -35,6 +21,25 @@ func random_level() int {
 	return level
 }
 
+type NodeInfo[K cmp.Ordered, T any] struct {
+	Node     *Node[K, T]
+	Distance uint
+}
+
+type Node[K cmp.Ordered, T any] struct {
+	Value T
+	Key   K
+	Next  []*NodeInfo[K, T]
+}
+
+func (n *Node[K, T]) String() string {
+	return fmt.Sprintf("[Node] level: %v value %v", n.Level(), n.Value)
+}
+
+func (n *Node[K, T]) Level() int {
+	return len(n.Next) - 1
+}
+
 type SkipList[K cmp.Ordered, T any] struct {
 	Head *Node[K, T]
 }
@@ -44,12 +49,20 @@ func (l *SkipList[K, T]) Debug() {
 		current_node := l.Head
 		for current_node != nil {
 			if current_node.Level() >= level {
-				fmt.Print(current_node.Key, "\t")
+				if current_node.Next[level] != nil {
+					fmt.Printf("%v(%v)\t", current_node.Value, current_node.Next[level].Distance)
+				} else {
+					fmt.Printf("%v\t", current_node.Value)
+				}
 			} else {
 				fmt.Print("\t")
 			}
 
-			current_node = current_node.Next[0]
+			if current_node.Next[0] != nil {
+				current_node = current_node.Next[0].Node
+			} else {
+				current_node = nil
+			}
 		}
 
 		fmt.Println()
@@ -62,44 +75,65 @@ func (l *SkipList[K, T]) Insert(key K, value T) {
 
 	// FIXME: Safeguard operation for uninitialized list
 	if l.Head == nil {
-		l.Head = &Node[K, T]{Next: make([]*Node[K, T], new_node_level+1)}
+		l.Head = &Node[K, T]{Next: make([]*NodeInfo[K, T], new_node_level+1)}
 	}
 
 	max_level := int(math.Max(float64(l.Head.Level()), float64(new_node_level)))
 
 	current_node := l.Head
-	nodes_to_update := make([]*Node[K, T], max_level+1) // <-- +1 because we need cap() to include index 0
+	nodes_to_update := make([]*Node[K, T], max_level+1)
+	distance_to_node := make([]uint, max_level+1)
 
 	// For each level from list Head top-most
 	for level := l.Head.Level(); level >= 0; level-- {
 		// Move right as much as possible
-		for current_node.Next[level] != nil && current_node.Next[level].Key <= key {
-			current_node = current_node.Next[level]
+		distance := uint(0)
+		for current_node.Next[level] != nil && current_node.Next[level].Node.Key <= key {
+			if current_node != l.Head {
+				distance++
+			}
+			current_node = current_node.Next[level].Node
 		}
 
 		// Store the stopping node to be updated
 		nodes_to_update[level] = current_node
+		distance_to_node[level] = distance
 	}
+	fmt.Printf("%v\n", distance_to_node)
 
 	// Create a new node
-	node_to_insert := &Node[K, T]{Key: key, Value: value, Next: make([]*Node[K, T], new_node_level+1)} // <-- +1 because we need cap() to include index 0
+	node_to_insert := &Node[K, T]{
+		Key: key, Value: value,
+		Next: make([]*NodeInfo[K, T], new_node_level+1),
+	}
 
 	// Mark for update higher levels of Head if necessary
 	if l.Head.Level() < new_node_level {
-		for lvl := new_node_level; lvl > l.Head.Level(); lvl-- {
-			nodes_to_update[lvl] = l.Head
+		for level := new_node_level; level > l.Head.Level(); level-- {
+			nodes_to_update[level] = l.Head
 		}
 
 		// And expand the Head Next slice
-		l.Head.Next = append(make([]*Node[K, T], 0, max_level+1), l.Head.Next...)
+		l.Head.Next = append(make([]*NodeInfo[K, T], 0, max_level+1), l.Head.Next...)
 	}
 
 	// Insert the node by updating the links
-	for level := new_node_level; level >= 0; level-- {
+	level_distance := uint(0)
+	for level := 0; level <= new_node_level; level++ {
 		node_to_update := nodes_to_update[level]
+		level_distance += distance_to_node[level]
+
 		if node_to_update.Level() >= level {
-			node_to_insert.Next[level] = node_to_update.Next[level]
-			node_to_update.Next[level] = node_to_insert
+			if node_to_update.Next[level] != nil {
+				node_to_insert.Next[level] = &NodeInfo[K, T]{
+					Node:     node_to_update.Next[level].Node,
+					Distance: node_to_update.Next[level].Distance - level_distance,
+				}
+			}
+			node_to_update.Next[level] = &NodeInfo[K, T]{
+				Node:     node_to_insert,
+				Distance: level_distance,
+			}
 		}
 	}
 }
@@ -112,29 +146,39 @@ func (l *SkipList[K, T]) Delete(key K) bool {
 
 	current_node := l.Head
 	nodes_to_update := make([]*Node[K, T], l.Head.Level()+1)
+	distance_to_node := make([]uint, l.Head.Level()+1)
 
 	// For each level from top-most
 	for level := l.Head.Level(); level >= 0; level-- {
 		// Move right as much as possible
-		for current_node.Next[level] != nil && current_node.Next[level].Key < key {
-			current_node = current_node.Next[level]
+		distance := uint(0)
+		for current_node.Next[level] != nil && current_node.Next[level].Node.Key <= key {
+			current_node = current_node.Next[level].Node
+			distance++
 		}
 
 		// And save node to update later
 		nodes_to_update[level] = current_node
+		distance_to_node[level] = distance
 	}
 
 	// Next node has to be be the node containing the value
-	node_to_delete := current_node.Next[0]
+	node_to_delete := current_node.Next[0].Node
 	if node_to_delete.Key != key {
 		return false
 	}
 
 	// Delete the node by updating the saved nodes
-	for level := l.Head.Level(); level >= 0; level-- {
+	level_distance := uint(0)
+	for level := 0; level <= l.Head.Level(); level++ {
 		node_to_update := nodes_to_update[level]
-		if node_to_delete.Level() >= level {
-			node_to_update.Next[level] = node_to_delete.Next[level]
+		level_distance += distance_to_node[level]
+
+		if node_to_delete.Level() >= level && node_to_update.Next[level] != nil {
+			node_to_update.Next[level] = &NodeInfo[K, T]{
+				Node:     node_to_delete.Next[level].Node,
+				Distance: node_to_update.Next[level].Distance + level_distance,
+			}
 		}
 	}
 
@@ -166,11 +210,36 @@ func (l *SkipList[K, T]) Find(key K) *T {
 	// For each level from top-most
 	for level := l.Head.Level(); level >= 0; level-- {
 		// Move right as much as possible
-		for current_node.Next[level] != nil && current_node.Next[level].Key <= key {
-			current_node = current_node.Next[level]
+		for current_node.Next[level] != nil && current_node.Next[level].Node.Key <= key {
+			current_node = current_node.Next[level].Node
 		}
 
 		if current_node.Key == key {
+			return &current_node.Value
+		}
+	}
+
+	return nil
+}
+
+func (l *SkipList[K, T]) At(index uint) *T {
+	// FIXME: Safeguard operation for uninitialized list
+	if l.Head == nil {
+		return nil
+	}
+
+	current_node := l.Head
+	distance := uint(0)
+
+	// For each level from top-most
+	for level := l.Head.Level(); level >= 0; level-- {
+		// Move right as much as possible
+		for current_node.Next[level] != nil && distance+current_node.Next[level].Distance <= index {
+			distance += current_node.Next[level].Distance
+			current_node = current_node.Next[level].Node
+		}
+
+		if distance == index {
 			return &current_node.Value
 		}
 	}
--- data_structures/skip_list/skip_list_test.go
+++ data_structures/skip_list/skip_list_test.go
@@ -79,3 +79,40 @@ func TestSkipList_Find(t *testing.T) {
 		t.Errorf("Expected value to be nil for key 4, got %v", value)
 	}
 }
+
+func TestSkipList_At(t *testing.T) {
+	skipList := &SkipList[int, int]{}
+
+	// Insert some values into the skip list
+	skipList.Insert(1, 10)
+	skipList.Insert(2, 20)
+	skipList.Insert(3, 30)
+	skipList.Insert(4, 0)
+	skipList.Insert(5, 5)
+	skipList.Insert(6, 15)
+	skipList.Insert(7, 25)
+	skipList.Insert(8, -5)
+	skipList.Debug()
+
+	// Test accessing elements at specific indices
+	if value := skipList.At(0); value == nil || *value != 0 {
+		t.Errorf("Expected value 0 at index 0, got %v", value)
+	}
+	if value := skipList.At(1); value == nil || *value != 10 {
+		t.Errorf("Expected value 10 at index 1, got %v", value)
+	}
+	if value := skipList.At(2); value == nil || *value != 20 {
+		t.Errorf("Expected value 20 at index 2, got %v", value)
+	}
+	if value := skipList.At(3); value == nil || *value != 30 {
+		t.Errorf("Expected value 30 at index 3, got %v", value)
+	}
+
+	// Test accessing elements at out-of-bounds indices
+	if value := skipList.At(4); value != nil {
+		t.Errorf("Expected nil value at index 4, got %v", value)
+	}
+	if value := skipList.At(5); value != nil {
+		t.Errorf("Expected nil value at index 5, got %v", value)
+	}
+}
